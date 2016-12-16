5e3c35e131af512575c31ceb34559285692a7b8c
Ivan Wang <ivanwang@augmentum.com.cn>
2016-12-16 17:58:08 +0800

    mid

--- src/om_tutorial/core.cljs
+++ src/om_tutorial/core.cljs
@@ -2,12 +2,33 @@
   (:require [goog.dom :as gdom]
             [om.next :as om :refer-macros [defui]]
             [om.dom :as dom]))
+(def app-state (atom {:count 0}))
 
 (defui HelloWorld
   Object
   (render [this]
-    (dom/div nil "Hello, world")))
+    (dom/div nil (get (om/props this) :title))))
+
 
 (def hello (om/factory HelloWorld))
 
-(js/ReactDOM.render (hello) (gdom/getElement "app"))
+(def reconciler
+  (om/reconciler {:state app-state}))
+
+(defui Counter
+  Object
+  (render [this]
+    (let [{:keys [count]} (om/props this)]
+      (dom/div nil
+        (dom/span nil (str "Count: " count))
+        (dom/button
+          #js {:onClick
+               (fn [e]
+                 (swap! app-state update-in [:count] inc))}
+          "Click me!")))))
+
+
+(om/add-root! reconciler
+  Counter (gdom/getElement "app"))
+
+; (js/ReactDOM.render (hello {:title "Hello, world"}) (gdom/getElement "app"))
